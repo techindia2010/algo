5a7d3d7954ec3896bbe11fafc10b0a8050d17ac1
Shailesh Patel <77503999+techindia2010@users.noreply.github.com>
2023-06-08 17:49:27 +0530

    Add files via upload

--- MeanRevBacktester.py
+++ MeanRevBacktester.py
@@ -2,50 +2,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import brute
+from itertools import product
 plt.style.use("seaborn")
 
 
 class MeanRevBacktester():
-    ''' Class for the vectorized backtesting of Mean Reversion-based trading strategies.
-
-    Attributes
-    ==========
-    symbol: str
-        ticker symbol with which to work with
-    SMA: int
-        time window for SMA
-    dev: int
-        distance for Lower/Upper Bands in Standard Deviation units
-    start: str
-        start date for data retrieval
-    end: str
-        end date for data retrieval
-    tc: float
-        proportional transaction costs per trade
-        
-    Methods
-    =======
-    get_data:
-        retrieves and prepares the data
-        
-    set_parameters:
-        sets one or two new parameters for SMA and dev
-        
-    test_strategy:
-        runs the backtest for the Mean Reversion-based strategy
-        
-    plot_results:
-        plots the performance of the strategy compared to buy and hold
-        
-    update_and_run:
-        updates parameters and returns the negative absolute performance (for minimization algorithm)
-        
-    optimize_parameters:
-        implements a brute force optimization for the two parameters
+    ''' Class for the vectorized backtesting of Bollinger Bands-based trading strategies.
     '''
     
     def __init__(self, symbol, SMA, dev, start, end, tc):
+        '''
+        Parameters
+        ----------
+        symbol: str
+            ticker symbol (instrument) to be backtested
+        SMA: int
+            moving window in bars (e.g. days) for SMA
+        dev: int
+            distance for Lower/Upper Bands in Standard Deviation units
+        start: str
+            start date for data import
+        end: str
+            end date for data import
+        tc: float
+            proportional transaction/trading costs per trade
+        '''
         self.symbol = symbol
         self.SMA = SMA
         self.dev = dev
@@ -54,27 +35,33 @@ class MeanRevBacktester():
         self.tc = tc
         self.results = None
         self.get_data()
+        self.prepare_data()
         
     def __repr__(self):
         rep = "MeanRevBacktester(symbol = {}, SMA = {}, dev = {}, start = {}, end = {})"
         return rep.format(self.symbol, self.SMA, self.dev, self.start, self.end)
         
     def get_data(self):
-        ''' Retrieves and prepares the data.
+        ''' Imports the data from intraday_pairs.csv (source can be changed).
         '''
-        raw = pd.read_csv("twenty_minutes.csv", parse_dates = ["time"], index_col = "time")
+        raw = pd.read_csv("intraday_pairs.csv", parse_dates = ["time"], index_col = "time")
         raw = raw[self.symbol].to_frame().dropna()
         raw = raw.loc[self.start:self.end]
         raw.rename(columns={self.symbol: "price"}, inplace=True)
         raw["returns"] = np.log(raw / raw.shift(1))
-        raw["SMA"] = raw["price"].rolling(self.SMA).mean()
-        raw["Lower"] = raw["SMA"] - raw["price"].rolling(self.SMA).std() * self.dev
-        raw["Upper"] = raw["SMA"] + raw["price"].rolling(self.SMA).std() * self.dev
         self.data = raw
-        return raw
+        
+    def prepare_data(self):
+        '''Prepares the data for strategy backtesting (strategy-specific).
+        '''
+        data = self.data.copy()
+        data["SMA"] = data["price"].rolling(self.SMA).mean()
+        data["Lower"] = data["SMA"] - data["price"].rolling(self.SMA).std() * self.dev
+        data["Upper"] = data["SMA"] + data["price"].rolling(self.SMA).std() * self.dev
+        self.data = data
         
     def set_parameters(self, SMA = None, dev = None):
-        ''' Updates parameters and resp. time series.
+        ''' Updates parameters (SMA, dev) and the prepared dataset.
         '''
         if SMA is not None:
             self.SMA = SMA
@@ -88,7 +75,7 @@ class MeanRevBacktester():
             self.data["Upper"] = self.data["SMA"] + self.data["price"].rolling(self.SMA).std() * self.dev
             
     def test_strategy(self):
-        ''' Backtests the trading strategy.
+        ''' Backtests the Bollinger Bands-based trading strategy.
         '''
         data = self.data.copy().dropna()
         data["distance"] = data.price - data.SMA
@@ -99,51 +86,58 @@ class MeanRevBacktester():
         data["strategy"] = data.position.shift(1) * data["returns"]
         data.dropna(inplace = True)
         
-        # determine when a trade takes place
+        # determine the number of trades in each bar
         data["trades"] = data.position.diff().fillna(0).abs()
         
-        # subtract transaction costs from return when trade takes place
+        # subtract transaction/trading costs from pre-cost return
         data.strategy = data.strategy - data.trades * self.tc
         
         data["creturns"] = data["returns"].cumsum().apply(np.exp)
         data["cstrategy"] = data["strategy"].cumsum().apply(np.exp)
         self.results = data
        
-        # absolute performance of the strategy
-        perf = data["cstrategy"].iloc[-1]
-        # out-/underperformance of strategy
-        outperf = perf - data["creturns"].iloc[-1]
+        perf = data["cstrategy"].iloc[-1] # absolute performance of the strategy
+        outperf = perf - data["creturns"].iloc[-1] # out-/underperformance of strategy
         
         return round(perf, 6), round(outperf, 6)
     
     def plot_results(self):
-        ''' Plots the cumulative performance of the trading strategy
-        compared to buy and hold.
+        ''' Plots the performance of the trading strategy and compares to "buy and hold".
         '''
         if self.results is None:
-            print("No results to plot yet. Run a strategy.")
+            print("Run test_strategy() first.")
         else:
             title = "{} | SMA = {} | dev = {} | TC = {}".format(self.symbol, self.SMA, self.dev, self.tc)
-            self.results[["creturns", "cstrategy"]].plot(title=title, figsize=(12, 8))
-        
-    def update_and_run(self, boll):
-        ''' Updates parameters and returns the negative absolute performance (for minimazation algorithm).
-
-        Parameters
-        ==========
-        Params: tuple
-            parameter tuple with SMA and dist
-        '''
-        self.set_parameters(int(boll[0]), int(boll[1]))
-        return -self.test_strategy()[0]
-    
+            self.results[["creturns", "cstrategy"]].plot(title=title, figsize=(12, 8))     
+   
     def optimize_parameters(self, SMA_range, dev_range):
-        ''' Finds global maximum given the parameter ranges.
+        ''' Finds the optimal strategy (global maximum) given the Bollinger Bands parameter ranges.
 
         Parameters
-        ==========
-        SMA_range, dist_range: tuple
+        ----------
+        SMA_range, dev_range: tuple
             tuples of the form (start, end, step size)
         '''
-        opt = brute(self.update_and_run, (SMA_range, dev_range), finish=None)
-        return opt, -self.update_and_run(opt)
+        
+        combinations = list(product(range(*SMA_range), range(*dev_range)))
+        
+        # test all combinations
+        results = []
+        for comb in combinations:
+            self.set_parameters(comb[0], comb[1])
+            results.append(self.test_strategy()[0])
+        
+        best_perf = np.max(results) # best performance
+        opt = combinations[np.argmax(results)] # optimal parameters
+        
+        # run/set the optimal strategy
+        self.set_parameters(opt[0], opt[1])
+        self.test_strategy()
+                   
+        # create a df with many results
+        many_results =  pd.DataFrame(data = combinations, columns = ["SMA", "dev"])
+        many_results["performance"] = results
+        self.results_overview = many_results
+                            
+        return opt, best_perf
+    
--- SMABacktester.py
+++ SMABacktester.py
@@ -2,77 +2,61 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import brute
+from itertools import product
 plt.style.use("seaborn")
 
 
 class SMABacktester():
     ''' Class for the vectorized backtesting of SMA-based trading strategies.
-
-    Attributes
-    ==========
-    symbol: str
-        ticker symbol with which to work with
-    SMA_S: int
-        time window in days for shorter SMA
-    SMA_L: int
-        time window in days for longer SMA
-    start: str
-        start date for data retrieval
-    end: str
-        end date for data retrieval
-    tc: float
-        proportional transaction costs per trade
-        
-        
-    Methods
-    =======
-    get_data:
-        retrieves and prepares the data
-        
-    set_parameters:
-        sets one or two new SMA parameters
-        
-    test_strategy:
-        runs the backtest for the SMA-based strategy
-        
-    plot_results:
-        plots the performance of the strategy compared to buy and hold
-        
-    update_and_run:
-        updates SMA parameters and returns the negative absolute performance (for minimization algorithm)
-        
-    optimize_parameters:
-        implements a brute force optimization for the two SMA parameters
     '''
     
-    def __init__(self, symbol, SMA_S, SMA_L, start, end, tc):
+    def __init__(self, symbol, SMA_S, SMA_L, start, end):
+        '''
+        Parameters
+        ----------
+        symbol: str
+            ticker symbol (instrument) to be backtested
+        SMA_S: int
+            moving window in bars (e.g. days) for shorter SMA
+        SMA_L: int
+            moving window in bars (e.g. days) for longer SMA
+        start: str
+            start date for data import
+        end: str
+            end date for data import
+        '''
         self.symbol = symbol
         self.SMA_S = SMA_S
         self.SMA_L = SMA_L
         self.start = start
         self.end = end
-        self.tc = tc
         self.results = None 
         self.get_data()
+        self.prepare_data()
         
     def __repr__(self):
         return "SMABacktester(symbol = {}, SMA_S = {}, SMA_L = {}, start = {}, end = {})".format(self.symbol, self.SMA_S, self.SMA_L, self.start, self.end)
         
     def get_data(self):
-        ''' Retrieves and prepares the data.
+        ''' Imports the data from forex_pairs.csv (source can be changed).
         '''
-        raw = pd.read_csv("twenty_minutes.csv", parse_dates = ["time"], index_col = "time")
+        raw = pd.read_csv("forex_pairs.csv", parse_dates = ["Date"], index_col = "Date")
         raw = raw[self.symbol].to_frame().dropna()
-        raw = raw.loc[self.start:self.end]
+        raw = raw.loc[self.start:self.end].copy()
         raw.rename(columns={self.symbol: "price"}, inplace=True)
         raw["returns"] = np.log(raw / raw.shift(1))
-        raw["SMA_S"] = raw["price"].rolling(self.SMA_S).mean()
-        raw["SMA_L"] = raw["price"].rolling(self.SMA_L).mean()
         self.data = raw
         
+    def prepare_data(self):
+        '''Prepares the data for strategy backtesting (strategy-specific).
+        '''
+        data = self.data.copy()
+        data["SMA_S"] = data["price"].rolling(self.SMA_S).mean()
+        data["SMA_L"] = data["price"].rolling(self.SMA_L).mean()
+        self.data = data
+        
     def set_parameters(self, SMA_S = None, SMA_L = None):
-        ''' Updates SMA parameters and resp. time series.
+        ''' Updates SMA parameters and the prepared dataset.
         '''
         if SMA_S is not None:
             self.SMA_S = SMA_S
@@ -82,57 +66,58 @@ class SMABacktester():
             self.data["SMA_L"] = self.data["price"].rolling(self.SMA_L).mean()
             
     def test_strategy(self):
-        ''' Backtests the trading strategy.
+        ''' Backtests the SMA-based trading strategy.
         '''
         data = self.data.copy().dropna()
         data["position"] = np.where(data["SMA_S"] > data["SMA_L"], 1, -1)
         data["strategy"] = data["position"].shift(1) * data["returns"]
         data.dropna(inplace=True)
-        
-        # determine when a trade takes place
-        data["trades"] = data.position.diff().fillna(0).abs()
-        
-        # subtract transaction costs from return when trade takes place
-        data.strategy = data.strategy - data.trades * self.tc
-        
         data["creturns"] = data["returns"].cumsum().apply(np.exp)
         data["cstrategy"] = data["strategy"].cumsum().apply(np.exp)
         self.results = data
-        # absolute performance of the strategy
-        perf = data["cstrategy"].iloc[-1]
-        # out-/underperformance of strategy
-        outperf = perf - data["creturns"].iloc[-1]
+       
+        perf = data["cstrategy"].iloc[-1] # absolute performance of the strategy
+        outperf = perf - data["creturns"].iloc[-1] # out-/underperformance of strategy
         return round(perf, 6), round(outperf, 6)
     
     def plot_results(self):
-        ''' Plots the cumulative performance of the trading strategy
-        compared to buy and hold.
+        ''' Plots the performance of the trading strategy and compares to "buy and hold".
         '''
         if self.results is None:
-            print("No results to plot yet. Run a strategy.")
+            print("Run test_strategy() first.")
         else:
-            title = "{} | SMA_S = {} | SMA_L = {} | TC = {}".format(self.symbol, self.SMA_S, self.SMA_L, self.tc)
+            title = "{} | SMA_S = {} | SMA_L = {}".format(self.symbol, self.SMA_S, self.SMA_L)
             self.results[["creturns", "cstrategy"]].plot(title=title, figsize=(12, 8))
-        
-    def update_and_run(self, SMA):
-        ''' Updates SMA parameters and returns the negative absolute performance (for minimazation algorithm).
-
-        Parameters
-        ==========
-        SMA: tuple
-            SMA parameter tuple
-        '''
-        self.set_parameters(int(SMA[0]), int(SMA[1]))
-        return -self.test_strategy()[0]
     
-    def optimize_parameters(self, SMA1_range, SMA2_range):
-        ''' Finds global maximum given the SMA parameter ranges.
+    def optimize_parameters(self, SMA_S_range, SMA_L_range):
+        ''' Finds the optimal strategy (global maximum) given the SMA parameter ranges.
 
         Parameters
-        ==========
-        SMA1_range, SMA2_range: tuple
+        ----------
+        SMA_S_range, SMA_L_range: tuple
             tuples of the form (start, end, step size)
         '''
-        opt = brute(self.update_and_run, (SMA1_range, SMA2_range), finish=None)
-        return opt, -self.update_and_run(opt)
-
+        
+        combinations = list(product(range(*SMA_S_range), range(*SMA_L_range)))
+        
+        # test all combinations
+        results = []
+        for comb in combinations:
+            self.set_parameters(comb[0], comb[1])
+            results.append(self.test_strategy()[0])
+        
+        best_perf = np.max(results) # best performance
+        opt = combinations[np.argmax(results)] # optimal parameters
+        
+        # run/set the optimal strategy
+        self.set_parameters(opt[0], opt[1])
+        self.test_strategy()
+                   
+        # create a df with many results
+        many_results =  pd.DataFrame(data = combinations, columns = ["SMA_S", "SMA_L"])
+        many_results["performance"] = results
+        self.results_overview = many_results
+                            
+        return opt, best_perf
+        
+     
